8e78fef9c289baae3761b4c996bfc649040da930
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-10-29 11:10:22 -0300

    Enabling services to be recognized by eureka server.

--- authorization-gateway-service/src/main/java/com/jwt/ags/AuthorizationGatewayServiceApplication.java
+++ authorization-gateway-service/src/main/java/com/jwt/ags/AuthorizationGatewayServiceApplication.java
@@ -2,8 +2,10 @@ package com.jwt.ags;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthorizationGatewayServiceApplication {
 
 	public static void main(String[] args) {
