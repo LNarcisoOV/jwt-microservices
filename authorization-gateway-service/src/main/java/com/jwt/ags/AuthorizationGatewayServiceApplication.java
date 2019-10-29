9185a8ef90ed64cb2d8a779b4e2b81ad0898bef0
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-10-29 10:38:26 -0300

    Adding Authorization gatewary service for authentication of users.

---
+++ authorization-gateway-service/src/main/java/com/jwt/ags/AuthorizationGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.ags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ authorization-gateway-service/src/test/java/com/jwt/ags/AuthorizationGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.ags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
