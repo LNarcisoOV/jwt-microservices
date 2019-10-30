739a0091b3bac15b3f3faab6004d0f1162c57dd7
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-10-30 10:32:26 -0300

    Changing the logic if the user isn't authorized.

--- authorization-gateway-service/src/main/java/com/jwt/ags/controller/AuthorizationController.java
+++ authorization-gateway-service/src/main/java/com/jwt/ags/controller/AuthorizationController.java
@@ -7,14 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.JsonSyntaxException;
 import com.jwt.ags.model.Credential;
 import com.jwt.ags.util.JwtUtil;
-
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
-import io.jsonwebtoken.security.SignatureException;
 
 @RestController
 @RequestMapping("/authorization")
@@ -22,17 +16,8 @@ public class AuthorizationController {
 
 	@PostMapping(value = "/authenticate")
 	public ResponseEntity<Credential> authenticate(@RequestBody String jwt) {
-		try {
-			Credential credential = JwtUtil.decode(jwt);
-			credential.setStatusCode(HttpStatus.OK.value());
-			credential.setReturnMessage("SUCCESS");
-
-			return new ResponseEntity<>(credential, HttpStatus.OK);
-		} catch (SignatureException | ExpiredJwtException | UnsupportedJwtException | MalformedJwtException
-				| IllegalArgumentException | JsonSyntaxException e) {
-			return new ResponseEntity<>(
-					new Credential(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()),
-					HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		Credential credential = JwtUtil.decode(jwt);
+		return new ResponseEntity<>(credential,
+				credential.getStatusCode() == 200 ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
--- authorization-gateway-service/src/main/java/com/jwt/ags/util/JwtUtil.java
+++ authorization-gateway-service/src/main/java/com/jwt/ags/util/JwtUtil.java
@@ -3,13 +3,18 @@ package com.jwt.ags.util;
 import java.security.Key;
 
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpStatus;
 
 import com.google.gson.Gson;
 import com.jwt.ags.model.Credential;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 
 public class JwtUtil {
 
@@ -20,14 +25,25 @@ public class JwtUtil {
 		return Jwts.builder().setPayload(json).signWith(key).compact();
 	}
 
-	public static Credential decode(String jwt) {
-		Base64 base64 = new Base64(true);
+	public static Credential decode(String jwt) throws SignatureException, ExpiredJwtException,
+			UnsupportedJwtException, MalformedJwtException, IllegalArgumentException {
+		Credential credential = new Credential();
+		try {
+			Base64 base64 = new Base64(true);
 
-		String[] splitedToken = jwt.split("\\.");
-		String header = new String(base64.decode(splitedToken[0]));
-		String body = new String(base64.decode(splitedToken[1]));
-		String signature = new String(base64.decode(splitedToken[2]));
+			String[] splitedToken = jwt.split("\\.");
+			String header = new String(base64.decode(splitedToken[0]));
+			String body = new String(base64.decode(splitedToken[1]));
+			String signature = new String(base64.decode(splitedToken[2]));
 
-		return new Gson().fromJson(body, Credential.class);
+			credential = new Gson().fromJson(body, Credential.class);
+			credential.setStatusCode(HttpStatus.OK.value());
+			credential.setReturnMessage("SUCCESS");
+			
+			return credential;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Credential(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+		}
 	}
 }
